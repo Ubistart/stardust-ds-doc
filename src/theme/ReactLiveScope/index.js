e7027285da188e5ba2b1e22d341d4fe008163932
LucielSantos <luciel.vitor.bs@gmail.com>
2022-08-16 21:10:35 -0300

    feat: add exemples of Button component

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,10 +1,13 @@
 import * as StardustComponents from '@stardust-ds/react';
 import React from 'react';
 
+import DownloadIcon from "@site/static/img/DownloadIcon";
+
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
+  DownloadIcon,
   ...StardustComponents,
 };
 export default ReactLiveScope;
